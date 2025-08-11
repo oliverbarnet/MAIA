ce4d624c111453a6f15c6a508caac91969325b5e
BloodRed3719 <138595384+BloodRed3719@users.noreply.github.com>
2025-08-11 09:37:22 -0400

    1.0.1
    Added V1.0.1
    Renamed V1.0.0 folder from "maia" to "maia1.0.0

---
+++ maia1.0.1/app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, request, jsonify
+import jetson_inference
+import jetson_utils
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+# Load models ONCE
+net_1 = jetson_inference.imageNet(argv=[
+    '--model=/home/nvidia4/jetson-inference/python/training/classification/models/tumor_dataset/resnet18.onnx',
+    '--input_blob=input_0',
+    '--output_blob=output_0',
+    '--labels=/home/nvidia4/jetson-inference/python/training/classification/data/tumor_dataset/labels.txt'
+])
+
+net_2 = jetson_inference.imageNet(argv=[
+    '--model=/home/nvidia4/jetson-inference/python/training/classification/models/TTR_dataset/resnet18.onnx',
+    '--input_blob=input_0',
+    '--output_blob=output_0',
+    '--labels=/home/nvidia4/jetson-inference/python/training/classification/data/TTR_dataset/labels.txt'
+])
+
+net_3 = jetson_inference.imageNet(argv=[
+    '--model=/home/nvidia4/jetson-inference/python/training/classification/models/alzheimers_dataset/resnet18.onnx',
+    '--input_blob=input_0',
+    '--output_blob=output_0',
+    '--labels=/home/nvidia4/jetson-inference/python/training/classification/data/alzheimers_dataset/labels.txt'
+])
+
+net_4 = jetson_inference.imageNet(argv=[
+    '--model=/home/nvidia4/jetson-inference/python/training/classification/models/covid19_detection/resnet18.onnx',
+    '--input_blob=input_0',
+    '--output_blob=output_0',
+    '--labels=/home/nvidia4/jetson-inference/python/training/classification/data/covid19_detection/labels.txt'
+])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    try:
+        file = request.files['image']
+        mode = request.form['mode']
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
+
+        img = jetson_utils.loadImage(filename)
+
+        if mode == '2':
+            net = net_1
+        elif mode == '3':
+            net = net_2
+        elif mode == '4':
+            net = net_3
+        elif mode == '5':
+            net = net_4
+        else:
+            return jsonify({'error': 'Invalid mode'}), 400
+
+        class_idx, confidence = net.Classify(img)
+        class_desc = net.GetClassDesc(class_idx)
+
+        return jsonify({
+            'prediction': class_desc,
+            'confidence': f"{confidence*100:.2f}%",
+            'id': class_idx
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
